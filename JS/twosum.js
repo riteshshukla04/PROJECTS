899c4d90d29bebbd559c42646b8f1007806bd33b
Ritesh <riteshshukla2381@gmail.com>
2022-10-01 16:08:49 +0530

    Two Sum code added

---
+++ JS/twosum.js
@@ -0,0 +1,14 @@
+var twoSum = function(nums, target) {
+    dict={}
+     for (let i=0;i<nums.length;i++){
+         if ((target-nums[i]) in dict){
+             return [dict[target-nums[i]],i];
+         }
+         else{
+             dict[nums[i]]=i;
+         }
+     }
+ 
+     
+     
+ };
